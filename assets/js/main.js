9a5c627c881fbf2093529f21c91c28657d498ef0
Ng-Vanh <nguyenvietanh2234@gmail.com>
2023-08-15 22:44:59 +0700

    reset grid and fix responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,16 @@ for(let i = 0; i < menuItemElements.length; i++){
         menuItemElements[i].classList.add("navbar__menu-item-actived")
     })
 }
+
+// click Menu Btn on Mobile or Tablet
+
+var menuElement = document.querySelector(".menu_MBTL i")
+var menuContentElement = document.querySelector(".menu_MBTL-content")
+
+menuElement.addEventListener("click", function(){
+    if(menuContentElement.style.display == "none"){
+        menuContentElement.style.display = "block";
+    }else{
+        menuContentElement.style.display = "none";
+    }
+})
